e024866c5aca4e882074f63b5496614ea80dd043
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-12-12 22:30:00 -0500

    updates seeds to add more participants to hackathon 1

--- database/seeds/03-user-hackathons.js
+++ database/seeds/03-user-hackathons.js
@@ -6,6 +6,8 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('user_hackathons').insert([
         {hackathon_id: 1, user_id: 1, user_role: 'judge'},
+        {hackathon_id: 1, user_id: 2, user_role: 'participant'},
+        {hackathon_id: 1, user_id: 3, user_role: 'participant'},
         {hackathon_id: 2, user_id: 2, user_role: 'organizer'},
         {hackathon_id: 3, user_id: 1, user_role: 'judge'},
         {hackathon_id: 4, user_id: 2, user_role: 'participant'},
